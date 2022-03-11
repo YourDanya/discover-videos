38d1c8df30513e956006d9f7614c5907d3d123ce
Danyl <pochtaformumumu@gmail.com>
2022-03-11 23:14:22 +0200

    new commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'i.ytimg.com']
+  },
+  env: {
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    MAGIC_PUBLISHABLE_API_KEY: process.env.MAGIC_PUBLISHABLE_API_KEY,
+    DEVELOPMENT: process.env.DEVELOPMENT
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import React, {useEffect, useState} from "react"
+import {useRouter} from "next/router"
+import {magic} from '../lib/magic-client'
+import Loading from "../components/loading/loading"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    const router= useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(async () => {
+        // const isLoggedIn = await magic.user.isLoggedIn()
+        // if (isLoggedIn) {
+        //     await router.push('/')
+        // } else {
+        //     await router.push('/login ')
+        // }
+    }, [])
+
+    useEffect(() => {
+        const handleComplete = () => {
+            setIsLoading(false)
+        }
+        router.events.on("routeChangeComplete", handleComplete)
+        router.events.on("routeChangeError", handleComplete)
+
+        return () => {
+            router.events.off("routeChangeComplete", handleComplete)
+            router.events.off("routeChangeError", handleComplete)
+        }
+    }, [router])
+
+    return isLoading? <Loading/> : <Component {...pageProps} />
 }
 
 export default MyApp
